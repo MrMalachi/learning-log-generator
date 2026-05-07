6cd5afe0ea920a994f26daaf94fd1aed08172be6
DJ Weeaboo <djweeaboo@MacBookAir.lan>
2026-05-07 00:48:47 -0400

    feat: implement main menu for user and add stub for log creation

--- src/learning_log_generator/learning_log_generator.py
+++ src/learning_log_generator/learning_log_generator.py
@@ -1,3 +1,5 @@
+
+
 class LearningLogGenerator:
     """
     A Python program that generates a structured daily learning log for my
@@ -7,10 +9,24 @@ class LearningLogGenerator:
     def __init__(self):
         """Initialize attributes."""
 
+    def main_menu(self):
+        """A neatly printed display of the learning log cli menu."""
+        print(
+            "\n============================================"
+            "\n          Learning Log Generator            "
+            "\n============================================"
+            "\n1. Create a new learning log"
+            "\n2. View saved learning logs"
+            "\n3. Search learning logs by keyword"
+            "\n4. Edit a learning log"
+            "\n5. Delete a learning log"
+            "\n6. Exit"
+        )
 
-
-
+    def create_learning_log(self):
+        """"""
+        pass
 
     def run_learning_log_generator(self):
         """The class' internal orchestrator."""
-        pass
+        self.main_menu()
