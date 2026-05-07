da4d82b7e5bafe6bd437523855f5b4609a61d4ab
DJ Weeaboo <djweeaboo@MacBookAir.lan>
2026-05-07 13:03:40 -0400

    feat: add exception handling to the get_main_menu_choice method

--- src/learning_log_generator/learning_log_generator.py
+++ src/learning_log_generator/learning_log_generator.py
@@ -1,3 +1,5 @@
+import subprocess
+import platform
 
 
 class LearningLogGenerator:
@@ -13,7 +15,7 @@ class LearningLogGenerator:
         """A neatly printed display of the learning log cli menu."""
         print(
             "\n============================================"
-            "\n          Learning Log Generator            "
+            "\n          🗒️Learning Log Generator🗒️            "
             "\n============================================"
             "\n1. Create a new learning log"
             "\n2. View saved learning logs"
@@ -23,6 +25,26 @@ class LearningLogGenerator:
             "\n6. Exit"
         )
 
+    def get_main_menu_choice(self):
+        """Prompt user to enter a corresponding integer & return value."""
+        while True:
+            try:
+                choice = int(input("\nEnter a number (1-6): "))
+
+                if choice < 1 or choice > 6:
+                    print("Please enter a valid number...")
+                    continue
+
+            except ValueError:
+                print("Please enter a number...")
+            else:
+                return choice
+
+
+
+
+
+
     def create_learning_log(self):
         """"""
         pass
@@ -30,3 +52,4 @@ class LearningLogGenerator:
     def run_learning_log_generator(self):
         """The class' internal orchestrator."""
         self.main_menu()
+        self.get_main_menu_choice()
