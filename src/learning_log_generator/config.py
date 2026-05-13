d5330d3c97efc4e1b08867b099df56303875922a
MrMalachi <brownmnb11@yahoo.com>
2026-05-12 21:13:32 -0400

    Remove unecessary file path with datetime import

--- src/learning_log_generator/config.py
+++ src/learning_log_generator/config.py
@@ -1,4 +1,5 @@
 from pathlib import Path
 
-
+# config.py file used for 'settings' or 'constants' that will probably not change during runtime.
 LOG_TEMPLATE_PATH = Path("src/learning_log_generator/templates/learning_log_template.md")
+LOGS_FOLDER = Path("data/logs")
