3de21b07f3dad41d95ec84e3f83453bc7b6dc42d
MrMalachi <brownmnb11@yahoo.com>
2026-05-21 20:06:10 -0400

    chore: add resource_path helper function to build a path to the bundled file inside PyInstaller's temporary folder or uses the normal path project so that templates bubdled resource works in both cases: normal Python project & PyInstaller executable

--- src/learning_log_generator/config.py
+++ src/learning_log_generator/config.py
@@ -1,4 +1,14 @@
 from pathlib import Path
+import sys
+
+
+def resource_path(relative_path):
+    """Get the correct file path for normal Python or PyInstaller."""
+    if hasattr(sys, "_MEIPASS"):
+        return Path(sys._MEIPASS / relative_path)
+
+    return Path(relative_path)
+
 
 # config.py file used for 'settings' or 'constants' that will probably not change during runtime.
 LOG_TEMPLATE_PATH = Path("src/learning_log_generator/templates/learning_log_template.md")
