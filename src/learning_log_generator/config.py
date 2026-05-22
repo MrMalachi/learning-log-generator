e655bb837750fc5da3686f590c89bb7325720519
MrMalachi <brownmnb11@yahoo.com>
2026-05-21 20:16:18 -0400

    chore: use the resource_path function on the LOGS_TEMPLATE_PATH so that if the executable is not being run, when the script is normally being run from the command line, the path still points to the correct template path. User-created logs like templates should not be saved inside PyInstaller's temporary folder because it can dissappear after the executable closes

--- src/learning_log_generator/config.py
+++ src/learning_log_generator/config.py
@@ -11,5 +11,5 @@ def resource_path(relative_path):
 
 
 # config.py file used for 'settings' or 'constants' that will probably not change during runtime.
-LOG_TEMPLATE_PATH = Path("src/learning_log_generator/templates/learning_log_template.md")
+LOG_TEMPLATE_PATH = resource_path("src/learning_log_generator/templates/learning_log_template.md")
 LOGS_FOLDER = Path("data/logs")
