d8c1ca561d6336f94b62e1fbd154a4347fc51025
MrMalachi <brownmnb11@yahoo.com>
2026-06-02 15:42:26 -0400

    refactor: restructured both constants above 'resource_path' function for readability. Also included a closed parenthesis around the path object 'sys._MEIPASS' because it is a path string

--- src/learning_log_generator/config.py
+++ src/learning_log_generator/config.py
@@ -2,14 +2,27 @@ from pathlib import Path
 import sys
 
 
+# Package folder:
+# /Users/malachibrown/dev/python_work/projects/learning-log-generator/src/learning_log_generator
+PACKAGE_DIR = Path(__file__).resolve().parent
+
+# Project root:
+# Users/malachibrown/dev/python_work/projects/learning-log-generator
+PROJECT_ROOT = PACKAGE_DIR.parent.parent
+
+
 def resource_path(relative_path):
     """Get the correct file path for normal Python or PyInstaller."""
     if hasattr(sys, "_MEIPASS"):
-        return Path(sys._MEIPASS / relative_path)
+        return Path(sys._MEIPASS) / relative_path
 
-    return Path(relative_path)
+    return PROJECT_ROOT / relative_path
 
 
-# config.py file used for 'settings' or 'constants' that will probably not change during runtime.
-LOG_TEMPLATE_PATH = resource_path("src/learning_log_generator/templates/learning_log_template.md")
-LOGS_FOLDER = Path("data/logs")
+# Template file location
+LOG_TEMPLATE_PATH = resource_path(
+    Path("src") / "learning_log_generator" / "templates" / "learning_log_template.md"
+)
+
+# Logs folder location
+LOGS_FOLDER = PROJECT_ROOT / "data" / "logs"
